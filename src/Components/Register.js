55d262d6bf98389a12365abb6085a9736a16e5a8
Sanath-Kumar-M-Kashyap <sanathkumar1708@gmail.com>
2023-11-01 23:19:08 +0530

    updated register form

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import backgroundImage from '../Assets/jogfalls.jpg';
-import { FaGoogle, FaLinkedin } from 'react-icons/fa'; // Import Google and LinkedIn icons from react-icons
-
+import { FaGoogle, FaLinkedin } from 'react-icons/fa';
 
 const Register = () => {
-  const loginURL = '/login';
+  const loginURL = '/register';
 
   const handleGoogleClick = () => {
     // Handle Google login logic
@@ -15,28 +14,41 @@ const Register = () => {
   };
 
   return (
-    <div className="register-container">
-      <div className="image">
-        <img src={backgroundImage} alt="Your Image" />
+    <div className="login-page">
+      <div className="login-left">
+        <img src={backgroundImage} alt="" />
       </div>
-      <div className="register-form">
-        <h2>Register</h2>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
-        <button className="blue-button">Register</button>
-        <div className="login-prompt">
-          Already have an account? <a href={loginURL} className="blue-link">Sign in</a>
-        </div>
-        <div className="oauth-icons">
-          <p>Or sign up with</p>
-          <a href="#" className="google-icon" onClick={handleGoogleClick}>
-            <FaGoogle size={24} />
-          </a>
-          <a href="#" className="linkedin-icon" onClick={handleLinkedInClick}>
-            <FaLinkedin size={24} />
-          </a>
+      
+      <div className="login-right">
+      <svg className='login-left-svg' width="100%" height="100%">
+
+   <pattern id="pattern-hex" x="0" y="0" width="112" height="190" patternUnits="userSpaceOnUse" viewBox="56 -254 112 190">
+     <g id="hexagon">
+       <path d="M168-127.1c0.5,0,1,0.1,1.3,0.3l53.4,30.5c0.7,0.4,1.3,1.4,1.3,2.2v61c0,0.8-0.6,1.8-1.3,2.2L169.3-0.3 c-0.7,0.4-1.9,0.4-2.6,0l-53.4-30.5c-0.7-0.4-1.3-1.4-1.3-2.2v-61c0-0.8,0.6-1.8,1.3-2.2l53.4-30.5C167-127,167.5-127.1,168-127.1 L168-127.1z"></path>
+       <path d="M112-222.5c0.5,0,1,0.1,1.3,0.3l53.4,30.5c0.7,0.4,1.3,1.4,1.3,2.2v61c0,0.8-0.6,1.8-1.3,2.2l-53.4,30.5 c-0.7,0.4-1.9,0.4-2.6,0l-53.4-30.5c-0.7-0.4-1.3-1.4-1.3-2.2v-61c0-0.8,0.6-1.8,1.3-2.2l53.4-30.5 C111-222.4,111.5-222.5,112-222.5L112-222.5z"></path>
+       <path d="M168-317.8c0.5,0,1,0.1,1.3,0.3l53.4,30.5c0.7,0.4,1.3,1.4,1.3,2.2v61c0,0.8-0.6,1.8-1.3,2.2L169.3-191 c-0.7,0.4-1.9,0.4-2.6,0l-53.4-30.5c-0.7-0.4-1.3-1.4-1.3-2.2v-61c0-0.8,0.6-1.8,1.3-2.2l53.4-30.5 C167-317.7,167.5-317.8,168-317.8L168-317.8z"></path>
+     </g>
+ 
+ </pattern>
+ <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern-hex)"></rect>
+</svg>
+        <div className="login-form-container">
+          <form className='login-form'>
+            <h2> Hello! Welcome!</h2> 
+            <input type= "text" name="" id="" placeholder= 'Enter your name'/>
+            <input type="text" name="" id="" placeholder='Enter your email'/>
+            <input type="password" name="" id="" placeholder='Enter your password'/>
+            <input type="password" name="" id="" placeholder='Confirm password'/>
+            <button className='login-btn btn primary-btn'>Register</button>
+            <p>Already have an account? <a href="/">Signin</a></p>
+            <div className="hr-div">
+              <hr /><p>or</p><hr />
+            </div>
+            <div className="social-logins">
+              <button className='social-signin-btn google-btn btn'><FaGoogle className='social-login-icon'/> Login With Google</button>
+              <button className='social-signin-btn linkedin-btn btn primary-btn'><FaLinkedin className='social-login-icon'/> Login With LinkedIn</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -44,7 +56,6 @@ const Register = () => {
 }
 
 export default Register;
-
 
 
 
