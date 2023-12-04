d24891884e2b67e45760502521555cda33ebb362
Sanath-Kumar-M-Kashyap <sanathkumar1708@gmail.com>
2023-12-04 22:08:56 +0530

    Added categories

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Hero from './Components/Hero';
 import Nav from './Components/Nav';
-//  import Login from './Components/Login'
-//  import Register from './Components/Register'
+ import Login from './Components/Login'
+ import Register from './Components/Register'
+import LandingCategories from './Components/LandingCategories';
 function App() {
   return (
     <div >
-      
+      <LandingCategories/>
        <Nav/> 
        <Hero/> 
         <Login/>  
