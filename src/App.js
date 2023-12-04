a393bb9ca798bd01314ef93dbc76c47ef17798b3
Sanath-Kumar-M-Kashyap <sanathkumar1708@gmail.com>
2023-12-04 20:38:39 +0530

    Local changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-// import Hero from './Components/Hero';
-// import Nav from './Components/Nav';
+import Hero from './Components/Hero';
+ import Nav from './Components/Nav';
  import Login from './Components/Login'
  import Register from './Components/Register'
 function App() {
   return (
     <div >
-      {/* <Nav/> */}
-      {/* <Hero/> */}
+       <Nav/> 
+       <Hero/> 
         <Login/>  
         <Register/>  
     </div>
